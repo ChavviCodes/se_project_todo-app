bcc822802f99d95b63bada5e39aec465d4a3457c
Chavvi <guitardude96@Hotmail.com>
2025-06-22 16:12:04 -1000

    Refactor Project using Object Oriented Programming v3

--- components/Popup.js
+++ components/Popup.js
@@ -2,36 +2,37 @@ class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
     this._popupCloseBtn = this._popupElement.querySelector(".popup__close");
-    // this._handleEscapeClose = _handleEscapeClose();
+    this._handleEscapeCloseWrapper = (evt) => {
+      this._handleEscapeClose(evt);
+    };
   }
 
   _handleEscapeClose(evt) {
     if (evt.key === "Escape") {
-     this.close();
-  }
+      this.close();
+    }
   }
 
   open() {
     this._popupElement.classList.add("popup_visible");
-    document.addEventListener("keyup", (evt) => {
-      this._handleEscapeClose(evt);
-    });
+    document.addEventListener("keyup", this._handleEscapeCloseWrapper);
   }
 
   close() {
     this._popupElement.classList.remove("popup_visible");
-    document.removeEventListener("keyup", (evt) => {
-      this._handleEscapeClose(evt);
-    });
+    document.removeEventListener("keyup", this._handleEscapeCloseWrapper);
   }
 
   setEventListeners() {
-    this._popupCloseBtn.addEventListener("click", () => {
-      this.close();
+    this._popupElement.addEventListener("mousedown", (evt) => {
+      if (
+        evt.target === evt.currentTarget ||
+        evt.target.classList.contains("popup__close")
+      ) {
+        this.close();
+      }
     });
-    this._popupElement.addEventListener("mousedown", this.close)
   }
-
 }
 
 export default Popup;
